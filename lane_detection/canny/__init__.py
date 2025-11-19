bc0979977f0e1eebdf964c25435b7270a2a33789
Usman <usman.muneer720@gmail.com>
2025-11-19 12:26:16 +0500

    relative import fix

--- lane_detection/canny/__init__.py
+++ lane_detection/canny/__init__.py
@@ -1,5 +1,5 @@
-from canny import canny_edge_detector
-from canny import apply_gaussian_filter
+from .canny import canny_edge_detector
+from .canny import apply_gaussian_filter
 
 __all__ = ['canny_edge_detector','apply_gaussian_filter']
 
