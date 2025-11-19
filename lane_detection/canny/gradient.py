46567cb41fd7b5870af0b7ca14095a8ac7fcd28a
Usman <usman.muneer720@gmail.com>
2025-11-19 12:16:36 +0500

    remove normailzation for better results

--- lane_detection/canny/gradient.py
+++ lane_detection/canny/gradient.py
@@ -9,13 +9,11 @@ class Gradient:
     def compute_magnitude(self):
         
         mag = np.sqrt(self.fx**2 + self.fy**2) / self.scale_factor
-
-        if mag.max() > 0:
-            mag = (mag / mag.max()) * 255
+        mag = np.clip(mag, 0, 255)
+        
         return mag.astype(np.uint8)
 
     def compute_direction(self):
         direction = np.arctan2(self.fy, self.fx) * 180 / np.pi
-
         direction = (direction + 360) % 360
         return direction
